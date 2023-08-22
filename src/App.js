c1e53c02af27049c094ef59b8bf273a9cf7199ce
Pdante1897 <bryanpaez.125@gmail.com>
2023-08-21 23:37:06 -0600

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [artist, setArtist] = useState('');
+  const [year, setYear] = useState('');
+  const [genre, setGenre] = useState('');
+  const [musicData, setMusicData] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = { titulo: title, artista: artist, anio: year, genero: genre };
+    
+    try {
+      await axios.post('http://localhost:8080/music', formData);
+      fetchMusicData();
+    } catch (error) {
+      console.error('Error sending data:', error);
+    }
+  };
+
+  const fetchMusicData = async () => {
+    try {
+      //const response = await axios.get('http://localhost:8080/music');
+      //setMusicData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMusicData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Música</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Título" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" placeholder="Artista" value={artist} onChange={(e) => setArtist(e.target.value)} />
+        <input type="number" placeholder="Año" value={year} onChange={(e) => setYear(e.target.value)} />
+        <input type="text" placeholder="Género" value={genre} onChange={(e) => setGenre(e.target.value)} />
+        <button type="submit">Enviar</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Artista</th>
+            <th>Año</th>
+            <th>Género</th>
+          </tr>
+        </thead>
+        <tbody>
+          {musicData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.title}</td>
+              <td>{item.artist}</td>
+              <td>{item.year}</td>
+              <td>{item.genre}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
